ac5507293bf9c640f23f99309847758cc7798a75
Станислав <@DESKTOP-T3UMIHD>
2025-01-23 17:02:44 +0700

    Добавьте файлы проекта.

---
+++ PR-22.101_Bakhturov_authorization/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PR_22._101_Bakhturov_authorization.Models;
+
+namespace PR_22._101_Bakhturov_authorization
+{
+    internal class Helper
+    {
+        public static KREntities GetContext()
+
+        { 
+
+            return new KREntities(); 
+        
+        }
+    }
+}
---
+++ PR-22.101_Bakhturov_authorization/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PR_22._101_Bakhturov_authorization.Models;
+using PR_22._101_Bakhturov_authorization;
+
+namespace PR_22._101_Bakhturov_authorization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            string login = txtbLogin.Text.Trim();
+            string password = pswbPassword.Text.Trim();
+            KREntities db = Helper.GetContext();
+            var user = db.Users.Where(x => x.Login == login && x.Password == password).FirstOrDefault();
+
+            if (user != null)
+            {
+                MessageBox.Show("Добро пожаловать! Ваша роль:" + user.Employees.Role);
+
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль", "Ошибка входа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void txtbLogin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+         
+        }
+
+        private void pswbPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
